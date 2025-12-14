f7298645a822abb540944ff72720a2c9d9999e0c
Narendra-v93 <narendravishvkarma5@gmail.com>
2025-12-14 15:06:40 +0530

    Fix function names and improve HTML structure

--- Assignment/jockesApp/script.js
+++ Assignment/jockesApp/script.js
@@ -1,4 +1,4 @@
-async function GetnewJock() {
+async function GetNewJoke() {
     const response = await fetch("https://official-joke-api.appspot.com/jokes/random");
     // console.log(response);
 
