66a7c7df7af44d5d63a8700af34f34ffeee37fb6
Narendra-v93 <narendravishvkarma5@gmail.com>
2025-12-22 15:26:23 +0530

    Enhance validation checks in submit function for user input fields

--- Assignment/InstituteRegi/script.js
+++ Assignment/InstituteRegi/script.js
@@ -9,17 +9,90 @@ function submit() {
     const gph = document.getElementById("GNumber").value.trim();   
 
     
-    // if(!/^[A-Za-z\s]+$/.test(nm)) { 
-    //     alert("Valid name only"); return; }
+    if(!/^[A-Za-z\s]+$/.test(nm))
+ { 
+        alert("Valid name only"); 
+        return; 
+    }
+ if(!nm)
+     { 
+        alert("Enter full name"); 
+        return;
+     }
+       
+    if(!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(em))
+     { 
+     alert("Valid Gmail only");
+     return;
+  }
+  if(!em)
+  {
+    alert("Enter email");
+    return;
+  }
+    if(!/^[6-9]\d{9}$/.test(ph))
+         { 
+        alert("10-digit mobile");
+         return; 
+        }
 
-    //     if(!nm) { 
-    //     alert("Enter full name"); return; }
-    // if(!/^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(em)) { 
-    //     alert("Valid Gmail only"); return; }
-    // if(!/^[6-9]\d{9}$/.test(ph)) { 
-    //     alert("10-digit mobile"); return; }
+        if(!ph)
+        {
+            alert("Enter mobile number");
+            return;
+        }
 
+//  if (!nm) {
+//     document.getElementById("NameError").innerText = "Required";
+//     return;
+//   } else if (!/^[A-Za-z ]+$/.test(nm)) {
+//     document.getElementById("NameError").innerText =
+//       "Only Alphabets and Spaces are Allowed";
+//     return;
+//   }
+//  if (!ph) {
+//     document.getElementById("PhoneError").innerText = "Required";
+//     return;
+//   } else if (!/^[6-9]\d{9}$/.test(ph)) {
+//     document.getElementById("PhoneError").innerText =
+//       "Only Indian Mobile Nummber allowed";
+//     return;
+//   }
+//   if (!em) {
+//     document.getElementById("EmailError").innerText = "Required";
+//     return;
+//   } else if (!/^[\w\.]+@(gmail|outlook|ricr|yahoo)\.(com|in|co.in)$/.test(em)) {
+//     document.getElementById("EmailError").innerText = "Use Proper Email Format";
+//     return;
+//   }
+//  if(!ct){
+//     alert("Enter city");
+//     return;
+//  } 
+
+//  if(!pn){
+//     alert("Enter PIN");
+//     return;
+//  }
+//  if (!gph) {
+//     document.getElementById("PhoneError").innerText = "Required";
+//     return;
+//   } else if (!/^[6-9]\d{9}$/.test(ph)) {
+//     document.getElementById("PhoneError").innerText =
+//       "Only Indian Mobile Nummber allowed";
+//     return;
+//   }
+  
+//   if(!cr)
+//   {
+//     alert("Select Course");
+//     return;
+//   }
+
+ 
     // Age check
+
+
     const today = new Date();
     const birthDate = new Date(db);
     let age = today.getFullYear() - birthDate.getFullYear();
@@ -27,15 +100,48 @@ function submit() {
         (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
         age--;
     }
-    if(age < 15) { 
-        alert("Age must be 15+"); return; }
+    if(age < 15) 
+        { 
+        alert("Age must be 15+");
+         return;
+         }
 
-    // if(!ct || !/^[a-zA-Z\s]+$/.test(ct)) { 
-    //     alert("Valid city"); return; }
-    // if(!pn || !/^[1-9]\d{5,6}$/.test(pn)) { 
-    //     alert("Valid PIN"); return; }
-    //  if(!gph ||  !/^[6-9]\d{9}$/.test(gph)) { 
-    //     alert("10-digit mobile"); return; }
+         if(!db)
+         {
+            alert("Enter DOB");
+            return;
+         }
+
+    if(!ct || !/^[a-zA-Z\s]+$/.test(ct)) 
+        { 
+        alert("Valid city");
+         return; 
+        }
+
+        if(!ct)
+        {
+            alert("Enter city");
+            return;
+        }
+
+
+
+    if(!pn || !/^[1-9]\d{5,6}$/.test(pn))
+         { 
+        alert("Valid PIN");
+         return;
+         }
+     if(!gph ||  !/^[6-9]\d{9}$/.test(gph))
+         { 
+        alert("10-digit mobile");
+         return;
+         }
+
+         if(!gph)
+         {
+            alert("Enter Guardian number");
+            return;
+         }
 
 {
     const data = {
