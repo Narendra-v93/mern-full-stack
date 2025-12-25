cfe097195e6c1e9c7e2159bb94e6d4b613b7db9e
Narendra-v93 <narendravishvkarma5@gmail.com>
2025-12-25 14:47:57 +0530

    Initialize React demo project with Vite, add basic app structure, styles, and SVG assets

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function App() {
+  let a = 5;
+  let b = 10;
+
+  console.log(a + b);
+  return (
+    <>
+      <section id="main">
+        <h1 id="head"><u><i>Hello Guys</i></u></h1>
+      <div id="text">This is my first React App</div>
+
+      <p id="lorem">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam
+        consequatur et ea blanditiis a maiores, animi rem impedit fuga nisi sint
+        eaque aspernatur nemo delectus, distinctio molestias? Perferendis,
+        expedita! Autem, corporis ipsam laboriosam quisquam eligendi facere
+        magni dignissimos consectetur, obcaecati nihil laudantium velit laborum
+        deserunt excepturi earum, qui consequatur ea fugit delectus? Veniam
+        minima consectetur officiis amet dolores in. Delectus doloribus
+        voluptatem facere? Voluptas maiores possimus quis facere quibusdam
+        consequatur eius unde voluptatem reiciendis repellat iste est, rerum
+        temporibus vel fugit amet autem quas soluta magni omnis? Dolores,
+        eligendi? Ducimus recusandae voluptatibus inventore velit natus nobis ut
+        dolores odio officia explicabo, aliquid ullam accusantium, iste
+        doloremque quis modi veniam nesciunt quibusdam! Perspiciatis veniam
+        nostrum consequatur qui. Architecto, a incidunt.
+      </p></section>
+    </>
+  );
+}
+
+export default App;
